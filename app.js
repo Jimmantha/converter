8ee8e7d08d1fca460ca53f104d2a4a39c8011d71
jimmantha <jimloy.22@ichat.sp.edu.sg>
2024-05-06 17:28:38 +0800

    starting point of overlay

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
 const path = require('path')
 const app = express()
-const port = 9000
+const port = 8000
 
 app.set('views', path.join(__dirname, 'website'));
 app.use(express.static(path.join(__dirname, 'website')));
 app.set('view engine', 'ejs');
 
+app.use(bodyparser.json())
+
 app.get('/', (req, res) => {
     res.render('converter')
+})
+
+app.post('/upload', (req, res) => {
+    console.log('upload read', req.body)
 })
 
 app.listen(port, () => {
     console.log('server running on port', port)
 })
+
